981257278ba8cbe1296f6a48cfae7ae424755ebc
Jack Sun <jsun1590@gmail.com>
2022-07-08 23:25:28 +0800

    init project files

---
+++ config.py
@@ -0,0 +1,14 @@
+from email.policy import default
+import os
+
+from decouple import config
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
+
+
+class Config(object):
+    FLASK_APP = config("SECRET_KEY", default="app.py")
+    SECRET_KEY = config("SECRET_KEY", default="BRUH-MOMENT")
+    SECRET_KEY = config("STEAM_API_KEY", default="STEAM")
