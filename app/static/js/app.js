3f31bb86668a2f44689fad454a2cf721abdc5913
Nicholas Choong <nicholascwc1997@gmail.com>
2022-07-10 12:43:56 +0800

    implemented word validation

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -9,6 +9,7 @@ const NUMBER_OF_GUESSES = 6;
 const NUMBER_OF_LETTERS = 5;
 
 let currentGuess = 0;
+let validWord = false;
 
 const initBoard = () => {
     let board = document.getElementById("board");
@@ -64,8 +65,39 @@ const displayWord = (data) => {
     suggestedWord.innerText = data;
 };
 
+// const checkWord = async () => {
+//     let payload2 = [];
+//     for (let i = 0; i < currentGuess + 1; i++) {
+//         let word = {};
+//         for (let j = 0; j < NUMBER_OF_LETTERS; j++) {
+//             word[j] = [
+//                 document.getElementById(`${i}-${j}`).innerText,
+//                 document.getElementById(`${i}-${j}`).getAttribute("bg"),
+//             ];
+//         }
+//         payload2.push(word);
+//     }
+
+//     payload2 = JSON.stringify(payload2);
+//     console.log(payload2);
+
+//     let res = await fetch("api/validate_word", {
+//         method: "POST",
+//         headers: {
+//             Accept: "application/json, text/plain, */*",
+//             "Content-Type": "application/json",
+//         },
+//         body: payload2,
+//     });
+
+//     let data = await res.json();
+//     console.log("data", await data);
+//     validWord = (await data) === "true";
+//     return validWord;
+// };
+
 const sendData = () => {
-    payload = [];
+    let payload = [];
     for (let i = 0; i < currentGuess; i++) {
         let word = {};
         for (let j = 0; j < NUMBER_OF_LETTERS; j++) {
@@ -141,11 +173,16 @@ enterText = (e) => {
             }
         }
         if (currentWord !== NUMBER_OF_LETTERS) return;
+        // if (!checkWord()) {
+        //     console.log("not a valid word");
+        //     console.log("check word", validWord);
+        //     return;
+        // }
         currentGuess++;
-
         sendData();
 
         activeGuess();
+        console.log("check word", validWord);
     }
 };
 initBoard();
