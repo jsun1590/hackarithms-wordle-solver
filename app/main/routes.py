d1e151873a26c02532119023c25c1d3d331864f2
Nicholas Choong <nicholascwc1997@gmail.com>
2022-07-10 10:56:04 +0800

    added guard cluases

--- app/main/routes.py
+++ app/main/routes.py
@@ -16,18 +16,14 @@ def index():
     return render_template("index.html")
 
 
-@bp.route("/api/validate_word", methods=["POST"])
-def validate_word():
-    word_list = request.get_json() or {}
-    recent_word = combine_letters(word_list[-1])
-    if not check_word(recent_word):
-        return jsonify({"error": "Not a word"}), 400
-
-
 @bp.route("/api/process_words", methods=["POST"])
 def process_words():
     word_list = request.get_json() or {}
     recent_word = combine_letters(word_list[-1])
+    if not check_word(recent_word):
+        response = jsonify("Not a word")
+        response.status_code = 201
+        return response
     recent_result = combine_results(word_list[-1])
     suggested_word = wordle_solver(recent_word, recent_result)
     response = jsonify(suggested_word)
